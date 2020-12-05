2618b3ee73c2727f55417005fa446e008c903665
Landon Whitesides <whitesideslandon@gmail.com>
2020-12-05 11:05:33 -0700

    added seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_172737) do
+ActiveRecord::Schema.define(version: 2020_12_05_174532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_12_05_172737) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema.define(version: 2020_12_05_172737) do
 
   add_foreign_key "addresses", "locations"
   add_foreign_key "locations", "trips"
+  add_foreign_key "trips", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+u = User.create(email:"test@test.com",password:123456)
+t = u.trips.create(title: "Andrew Family Vacation", date: "2020-12-05 17:40:31", description: "A really fun getaway")
+l = t.locations.create(name: "California", attraction: "Disneyland")
+Address.create(street: "disneyland way 123", city: "anaheim", state: "CA", zipcode: 78564,location_id:l.id)
+
+# 3.times do Trip.create(
+#     title:
+#     date:
+#     description:
+#     user_id: u.id
+# )
+# end
+
+# 2.times do 
+#   Location.create (
+#     name: 
+#     attraction:
+#   )
+# end
+
+# Address.create(
+#   street:
+#   city:
+#   state:
+#   zipcode:
+# )
